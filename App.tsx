4bac017501a24baf4a77aabbca9f5de60738b552
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-12-15 22:35:34 -0300

    Creating the scheduling screen and adding the calendar component

--- App.tsx
+++ App.tsx
@@ -13,9 +13,8 @@ import {
   Archivo_600SemiBold
 } from '@expo-google-fonts/archivo';
 
-import { Home } from './src/screens/Home';
+import { Scheduling } from './src/screens/Scheduling';
 import theme from './src/styles/theme';
-import { CarDetails } from './src/screens/CarDetails';
 
 export default function App() {
 
@@ -33,7 +32,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
   );
 }
---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,69 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { useTheme } from 'styled-components';
+
+import { BackButton } from '../../components/BackButton';
+import { Calendar } from '../../components/Calendar';
+import { Button } from '../../components/Button';
+
+import ArrowSvg from '../../assets/arrow.svg';
+
+import {
+ Container,
+ Header,
+ Title,
+ RentalPeriod,
+ DateInfo,
+ DateTitle,
+ DateValue,
+ Content,
+ Footer,
+} from './styles';
+
+export function Scheduling(){
+    const theme = useTheme();
+
+ return (
+    <Container>
+        <StatusBar
+            style='light'
+        />
+        <Header>
+            <BackButton onPress={() => { }}
+                color={theme.colors.shape}
+                style={{
+                    width: 24,
+                    height: 24,
+                }}
+            />
+            <Title>
+                Escolha uma{'\n'}
+                data de início e{'\n'}
+                fim do aluguel
+            </Title>
+
+            <RentalPeriod>
+                <DateInfo>
+                    <DateTitle>DE</DateTitle>
+                    <DateValue selected={false}>18/06/2021</DateValue>
+                </DateInfo>
+
+                <ArrowSvg />
+
+                <DateInfo>
+                    <DateTitle>ATÉ</DateTitle>
+                    <DateValue selected={false}>18/06/2021</DateValue>
+                </DateInfo>
+            </RentalPeriod>
+        </Header>
+
+        <Content>
+                <Calendar />
+        </Content>
+
+        <Footer>
+            <Button title="Confirmar" />
+        </Footer>
+    </Container>
+ );
+}
