7491dfb1e3481ef87c406c69197e0bf2c61242d9
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-12-19 00:40:22 -0300

    Refactor Inputs

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -29,8 +29,10 @@ export function Input({ iconName, value, ...rest }: Props){
     }
 
     return (
-        <Container isFocused={isFocused}>
-            <IconContainer>
+        <Container>
+            <IconContainer
+                isFocused={isFocused}
+            >
                 <Feather 
                     name={iconName}
                     size={24}
@@ -39,6 +41,7 @@ export function Input({ iconName, value, ...rest }: Props){
             </IconContainer>
 
             <InputText
+                isFocused={isFocused}
                 onFocus={handleInputFocus}
                 onBlur={handleInputBlur}
                 {...rest}
