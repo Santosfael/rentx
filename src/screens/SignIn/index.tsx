658e0c4a84907c3df934c6f4e7a918af284b3639
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-12-20 11:31:31 -0300

    Installation, configuration of watermelon on android and structuring the users database

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Alert, Keyboard, KeyboardAvoidingView } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { useTheme } from 'styled-components';
@@ -9,21 +9,22 @@ import * as Yup from 'yup';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
+import { database } from '../../database';
 import { useAuth } from '../../hooks/auth';
 
 import {
- Container, 
- Header,
- SubTitle,
- Title,
- Form,
- Footer,
+    Container,
+    Header,
+    SubTitle,
+    Title,
+    Form,
+    Footer,
 } from './styles';
 
-export function SignIn(){
+export function SignIn() {
     const theme = useTheme();
-    const [ email, setEmail ] = useState('');
-    const [ password, setPassword ] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const { navigate } = useNavigation<any>();
     const { signIn } = useAuth();
 
@@ -31,19 +32,19 @@ export function SignIn(){
         try {
             const schema = Yup.object().shape({
                 email: Yup.string()
-                .required("E-mail obrigatório")
-                .email('Digite um e-mail válido'),
-    
+                    .required("E-mail obrigatório")
+                    .email('Digite um e-mail válido'),
+
                 password: Yup.string()
-                .required('A senha é obrigatória')
-                .min(6, 'A senha tem no mínimo de 6 caracteres')
+                    .required('A senha é obrigatória')
+                    .min(6, 'A senha tem no mínimo de 6 caracteres')
             });
-    
-            await schema.validate({email, password});
+
+            await schema.validate({ email, password });
 
             signIn({ email, password });
         } catch (error) {
-            if(error instanceof Yup.ValidationError) {
+            if (error instanceof Yup.ValidationError) {
                 Alert.alert('Opa', error.message);
             } else {
                 Alert.alert('Erro na autenticação', 'Ocorreu um erro ao fazer login, verifique as credenciais');
@@ -55,6 +56,16 @@ export function SignIn(){
     function handleNewAccount() {
         navigate("SignUpFirstStep")
     }
+
+    useEffect(() => {
+        async function loadData() {
+            const usersCollection = database.get('users');
+            const users = await usersCollection.query().fetch();
+
+            console.log(users);
+        }
+        loadData();
+    }, [])
 
     return (
         <KeyboardAvoidingView
@@ -70,7 +81,7 @@ export function SignIn(){
                     </Header>
 
                     <Form>
-                        <Input 
+                        <Input
                             iconName='mail'
                             placeholder='E-mail'
                             keyboardType='email-address'
@@ -80,7 +91,7 @@ export function SignIn(){
                             value={email}
                         />
 
-                        <PasswordInput 
+                        <PasswordInput
                             iconName='lock'
                             placeholder='Senha'
                             autoCorrect={false}
@@ -90,14 +101,14 @@ export function SignIn(){
                     </Form>
 
                     <Footer>
-                        <Button 
+                        <Button
                             title='Login'
                             onPress={handleSignIn}
                             style={{ opacity: (!!email && !!password) ? 1 : 0.5 }}
                             enabled={(!!email && !!password) ? true : false}
                         />
 
-                        <Button 
+                        <Button
                             title='Criar conta gratuita'
                             color={theme.colors.background_secondary}
                             onPress={handleNewAccount}
